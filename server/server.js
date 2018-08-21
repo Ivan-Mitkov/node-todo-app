55cb7bdb938dbd16948e21bb0911e977968b7eee
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-08-21 07:45:09 +0300

    add validation

--- server/server.js
+++ server/server.js
@@ -11,37 +11,70 @@ mongoose.connect(url);
 //create mongoose model so that mongoose know how to store our data
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 //create new instance of the model
-let newTodo = new Todo({ text: 'Cook dinner' });
+// let newTodo = new Todo({ text: '  Learn to Spanish   ' });
 
-//saving the new Todo
-newTodo.save()
-    .then((doc) => {
-        console.log("Save Todo ", doc);
-    }).catch((e) => {
-        console.log('Unable to save Todo ', e);
-    });
+// //saving the new Todo
+// newTodo.save()
+//     .then((doc) => {
+//         console.log("Save Todo ", doc);
+//     }).catch((e) => {
+//         console.log('Unable to save Todo ', e);
+//     });
 
-let otherTodo = new Todo({
-    text: 'Learn MERN stack',
-    completed: true,
-    completedAt: 12345
-})
+// let otherTodo = new Todo({
+//     text: 'Learn MERN stack',
+//     completed: true,
+//     completedAt: 12345
+// })
 
-otherTodo.save()
-    .then((doc) => {
-        console.log("Saved ", doc);
-    })
-    .catch((e) => {
-        console.log(e);
-    });
+// otherTodo.save()
+//     .then((doc) => {
+//         console.log("Saved ", doc);
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     });
+
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        minLength: 5,
+        trim: true,
+        required: true
+    }
+});
+
+// let newUser = new User({
+//     email: 'As@k.com'
+// });
+
+// newUser.save()
+//     .then((doc) => {
+//         console.log(doc);
+//     }).catch((e) => {
+//         console.log(e);
+//     })
+
+// let checkUser = new User({ email: '' });
+// checkUser.save()
+//     .then((doc) => {
+//         console.log(doc);
+//     }).catch((e) => {
+//         console.log(e);
+//     });
