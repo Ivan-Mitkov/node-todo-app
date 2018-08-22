aea24732985424b40869bf5350fa2529576441c0
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-08-22 07:50:09 +0300

    add GET/todos route

--- server/server.js
+++ server/server.js
@@ -1,37 +1,46 @@
-const express=require('express');
-const bodyParser= require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const {mongoose}=require('./db/mongoose.js');
-const {Todo} =require('./models/todo.js');
-const {User} =require('./models/user.js');
+const { mongoose } = require('./db/mongoose.js');
+const { Todo } = require('./models/todo.js');
+const { User } = require('./models/user.js');
 
-const port=process.env.PORT||3000;
-let app=express();
+const port = process.env.PORT || 3000;
+let app = express();
 
 //configure middleware 
 app.use(bodyParser.json());
 
 //set up a route passing our url and a callback. URL is important /todo is for creating 
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
     //for test on postman only
     // console.log(req.body);
-    let todo=new Todo({
-        text:req.body.text
+    let todo = new Todo({
+        text: req.body.text
     });
     //saving to the database
     todo.save()
-    .then((doc)=>{
-        //send back the response
-        res.send(doc);
-    })
-    .catch((e)=>res.status(400).send(e));
+        .then((doc) => {
+            //send back the response
+            res.send(doc);
+        })
+        .catch((e) => res.status(400).send(e));
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find()
+    .then((todo)=>{
+        res.send({todo})
+    })
+    .catch((e)=>{
+        res.status(400).send(e);
+    })
+})
 
-if(!module.parent){ 
-    app.listen(port,()=>{
+if (!module.parent) {
+    app.listen(port, () => {
         console.log(port);
     });
 }
 
-module.exports={app}
+module.exports = { app }
