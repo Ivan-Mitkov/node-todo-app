fb86bc5d7b744042c13b1eef7e09ccd9f10ef611
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-08-22 19:12:47 +0300

    fix bug with connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,10 +6,10 @@ mongoose.Promise = global.Promise;
 // Connection URL
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mongodb:"ivan.mitkov:Ivan749343@ds125372.mlab.com:25372/node_complete"
-  };
-  mongoose.connect( db.localhost || db.mlab);
+    mongodb: "ivan.mitkov:Ivan749343@ds125372.mlab.com:25372/node_complete"
+};
 
-mongoose.connect(url, { useNewUrlParser: true });
+
+mongoose.connect(db.localhost || db.mongodb, { useNewUrlParser: true });
 
 module.exports = { mongoose };
