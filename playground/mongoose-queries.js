ab42cd03102539d31d896597725c1c7b9bb47d22
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-08-22 08:37:18 +0300

    add more quering methods in playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,33 @@
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const {ObgectId} =require('mongodb');
+
+let id = "5b7cf0952c9c47237432a967";
+
+if(!ObgectId.isValid(id)){
+    console.log(`Id ${id} is not valid`);
+}
+
+
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log(todos);
+});
+
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log(todo);
+});
+
+Todo.findById(id)
+    .then((todo) => {
+        if(!todo){
+            console.log('Id not found');
+        }
+        console.log(todo);
+    })
+    .catch((e)=>{
+        throw(e);
+    });
