81b4293009b8c0782b4a5c4c08effa15b045b65e
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-08-22 19:04:39 +0300

    set up for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connection URL
-const url = 'mongodb://localhost:27017/TodoApp';
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mongodb:"ivan.mitkov:Ivan749343@ds125372.mlab.com:25372/node_complete"
+  };
+  mongoose.connect( db.localhost || db.mlab);
 
 mongoose.connect(url, { useNewUrlParser: true });
 
